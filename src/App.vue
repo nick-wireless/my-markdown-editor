9238a34012cb9fe80147cd0fa7f6853ea6e180f5
Nick Hanigan <australian.nick@gmail.com>
2021-02-09 20:28:27 +0930

    Timeline
    Setup a master navigation.
    Hardcode for beginning and link together.

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
-  <div>Vue app</div>
+  <section class="section">
+    <div class="container">
+      <Timeline></Timeline>
+    </div>
+  </section>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import Timeline from './Timeline.vue'
 export default defineComponent({
-  name: 'App'
+  name: 'App',
+  components: {
+    Timeline
+  }
 })
 </script>
---
+++ src/Timeline.vue
@@ -0,0 +1,21 @@
+<template>
+    <nav class="is-primary panel">
+        <p class="panel-tabs">
+            <a>today</a>
+            <a>this week</a>
+            <a>this month</a>
+        </p>
+    </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue' // allows type definitions within the component.
+export default defineComponent({
+    
+})
+
+</script>
+
+<style scoped>
+
+</style>
