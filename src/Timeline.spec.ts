055d897942e76c6ccd96a6ff6dd67977b1646fc8
Nick Hanigan <australian.nick@gmail.com>
2021-02-10 22:03:09 +0930

    Testing Detailed Examples
    Detailed approach to changing Dom elements.

--- src/Timeline.spec.ts
+++ src/Timeline.spec.ts
@@ -1,4 +1,5 @@
 import { mount } from '@vue/test-utils'
+import { nextTick } from 'vue'
 import Timeline from './Timeline.vue'
 
 describe('Timeline', () => {
@@ -7,9 +8,25 @@ describe('Timeline', () => {
     expect(wrapper.findAll('[ data-test="period"]')).toHaveLength(3)
   })
 
-  it('updates the period when clicked', () => {
+  it('updates the period when clicked', async () => {
     const wrapper = mount(Timeline)
-    expect(wrapper.findAll('[data-test="period"]')[0].classes()).toContain('is-active')
+
+    const $today = wrapper.findAll('[data-test="period"]')[0]
+    expect($today.classes()).toContain('is-active')
+
+    const $thisWeek = wrapper.findAll('[data-test="period"]')[1]
+    await $thisWeek.trigger('click')
+
+    expect($today.classes()).not.toContain('is-active')
+    expect($thisWeek.classes()).toContain('is-active')
+    
+    const $thisMonth = wrapper.findAll('[data-test="period"]')[2]
+    await $thisMonth.trigger('click')
+    await nextTick()
+
+    expect($thisWeek.classes()).not.toContain('is-active')
+    expect($thisMonth.classes()).toContain('is-active')
+    
     })
   })
  
